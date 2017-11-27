668d262fbb0fd3efac95e29c4ca5325976d03686
Ferdinand Jason G <ferdinandjasong@gmail.com>
2017-11-27 10:15:36 +0700

    [touch.c] with xv6 style

--- touch.c
+++ touch.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+#include "fs.h"
 
 int main(int argc,char *argv[])
 {
     if(argc<2)
     {
-        printf(2,"Usage: touch files...\n");
-        return 0;
+        printf(1,"Usage: touch files...\n");
+        exit();
     }
     int i,err;
     for(i=1;i<argc;i++)
     {
-        err=open(argv[i],O_RDWR|O_CREAT,0644);
-        if(err==-1)
+        if((err=open(argv[i],O_CREATE|O_RDWR)) < 0)
         {
-            printf(2,"Error where creating %s\n",argv[i]);
-            return -1;
+            printf(1,"touch: error where creating %s\n",argv[i]);
+            exit();
         }
         close(err);
     }
-    return 0;
+    exit();
 }
