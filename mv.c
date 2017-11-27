fb3352951327427638b9c74b8311afa4dccba7ed
Ferdinand Jason G <ferdinandjasong@gmail.com>
2017-11-28 00:01:38 +0700

    [mv,c with xv6 style]

--- mv.c
+++ mv.c
@@ -5,6 +5,15 @@
 
 char buf[512];
 
+char* strncpy(char *s, const char *t, int n)
+{
+  int i;
+  char *os;
+  os = s;
+  for(i=0;i<n;i++) {s[i]=t[i];}
+  return os;
+}
+
 char* strcat(char* s1,char *s2)
 {
     char *b=s1;
@@ -22,23 +31,28 @@ int main(int argc,char *argv[])
         printf(1,"Need 2 arguments!\n");
         exit();
     }
+    printf(1,"%s\n",argv[1]);
     if((fd0=open(argv[1],O_RDONLY))<0)
     {
-        printf("mv: cannot open %s\n",argv[1]);
+        printf(2,"mv: cannot open %s\n",argv[1]);
         exit();
     }
-    strcat(argv[2],"/");
-    strcat(argv[2],argv[1]);
+    char temp[512];
+    strncpy(temp,argv[1],strlen(argv[1]));
+    if(unlink(strcat("/",argv[1]))<0)
+    {
+        printf(2,"gagal %s\n",strcat("/",argv[1]));
+    }
+    strcat(argv[2],temp);
     if ((fd1=open(argv[2],O_CREATE|O_RDWR))<0)
     {
-        printf("mv: cannot open %s\n",argv[2]);
+        printf(2,"mv: cannot open %s\n",argv[2]);
         exit();
     }
-    while( (n=read(fd0,buf,sizeof(buff)))>0 )
+    while( (n=read(fd0,buf,sizeof(buf)))>0 )
     {
         write(fd1,buf,n);
     }
-    unlink(argv[1]);
     close(fd0);
     close(fd1);
     exit();
