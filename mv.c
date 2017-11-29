a3030b1958f6e235aca04db850f3206369ec792c
Ferdinand Jason G <ferdinandjasong@gmail.com>
2017-11-29 23:38:36 +0700

    [mv.c] with * options or recursive

--- mv.c
+++ mv.c
@@ -4,122 +4,138 @@
 #include "fcntl.h"
 #include "fs.h"
 
-char*
+char *
 fmtname(char *path)
 {
-  static char buf[DIRSIZ+1];
+  static char buf[DIRSIZ + 1];
   char *p;
 
   // Find first character after last slash.
-  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+  for (p = path + strlen(path); p >= path && *p != '/'; p--)
     ;
   p++;
 
   // Return blank-padded name.
-  if(strlen(p) >= DIRSIZ)
+  if (strlen(p) >= DIRSIZ)
     return p;
   memmove(buf, p, strlen(p));
   //memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
   return buf;
 }
 
-char* strncpy(char *s, const char *t, int n)
+char *strncpy(char *s, const char *t, int n)
 {
   int i;
   char *os;
   os = s;
-  for(i=0;i<n;i++) {s[i]=t[i];}
+  for (i = 0; i < n; i++)
+  {
+    s[i] = t[i];
+  }
   return os;
 }
 
-char* strcat(char* s1,char *s2)
+char *strcat(char *s1, char *s2)
 {
-    char *b=s1;
-    while(*s1) ++s1;
-    while(*s2) *s1++ = *s2++;
-    *s1=0;
-    return b;
+  char *b = s1;
+  while (*s1)
+    ++s1;
+  while (*s2)
+    *s1++ = *s2++;
+  *s1 = 0;
+  return b;
 }
 
-void mv(char *argv1,char *argv2)
+void mv(char *argv1, char *argv2)
 {
-    //printf(1,"%s %s\n",argv1,argv2);
-    char buf[512];
-    int fd0,fd1,n;
-    if((fd0=open(argv1,O_RDONLY))<0)
-    {
-        printf(2,"mv: cannot open %s\n",argv1);
-        exit();
-    }
-    char temp[512];
-    strncpy(temp,argv1,strlen(argv1));
-    if(unlink(argv1)<0)
-    {
-        printf(2,"error moving %s\n",argv1);
-        exit();
-    }
-    strcat(argv2,temp);
-    if ((fd1=open(argv2,O_CREATE|O_RDWR))<0)
-    {
-        printf(2,"mv: cannot open %s\n",argv2);
-        exit();
-    }
-    while( (n=read(fd0,buf,sizeof(buf)))>0 )
-    {
-        write(fd1,buf,n);
-    } 
-    close(fd0);
-    close(fd1);
+  // printf(2,"MASUK\n");
+  // printf(1,"1 %s\n",argv1);
+  // printf(1,"2 %s\n",argv2);
+  // printf(1,"mv : %s %s\n",argv1,argv2);
+  if(argv1[0]=='.' && argv1[1]=='.') return;
+  char buf[512];
+  int fd0, fd1, n;
+  if ((fd0 = open(argv1, O_RDONLY)) < 0)
+  {
+    printf(2, "mv: cannot open %s\n", argv1);
+    exit();
+  }
+  char temp[512];
+  strncpy(temp, argv1, strlen(argv1));
+  if (unlink(argv1) < 0)
+  {
+    printf(2, "error moving %s\n", argv1);
+    exit();
+  }
+  if(temp[0]=='.' && temp[strlen(temp)-1]!='/' && argv2[strlen(argv2)-1]=='/' ) strcat(argv2,temp+2);
+  else if(temp[strlen(temp)-1]!='/' && argv2[strlen(argv2)-1]=='/') strcat(argv2, temp);
+  if ((fd1 = open(argv2, O_CREATE | O_RDWR)) < 0)
+  {
+    printf(2, "mv: cannot open %s\n", argv2);
+    exit();
+  }
+  while ((n = read(fd0, buf, sizeof(buf))) > 0)
+  {
+    write(fd1, buf, n);
+  }
+  close(fd0);
+  close(fd1);
 }
 
-void
-ls(char *path,char *argv2,int longz,char *hehe)
+void ls(char path[512], char argv2[512], int longz, char hehe[512])
 {
-  char buf[512],temp[512], *p;
+  char buf[512], temp[512], *p;
   int fd;
   struct dirent de;
   struct stat st;
 
-  if((fd = open(path, 0)) < 0){
+  if ((fd = open(path, 0)) < 0)
+  {
     printf(2, "ls: cannot open %s\n", path);
     return;
   }
 
-  if(fstat(fd, &st) < 0){
+  if (fstat(fd, &st) < 0)
+  {
     printf(2, "ls: cannot stat %s\n", path);
     close(fd);
     return;
   }
 
-  switch(st.type){
+  switch (st.type)
+  {
   case T_FILE:
     //printf(1, "%s %d %d %d\n", fmtname(path), st.type, st.ino, st.size);
     break;
 
   case T_DIR:
-    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+    if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf)
+    {
       printf(1, "ls: path too long\n");
       break;
     }
     strcpy(buf, path);
-    p = buf+strlen(buf);
+    p = buf + strlen(buf);
     *p++ = '/';
-    while(read(fd, &de, sizeof(de)) == sizeof(de)){
-      if(de.inum == 0)
+    while (read(fd, &de, sizeof(de)) == sizeof(de))
+    {
+      if (de.inum == 0)
         continue;
-      memset(temp,'\0',sizeof(temp));
-      strcpy(temp,argv2);
+      memset(temp, '\0', sizeof(temp));
+      strcpy(temp, argv2);
       memmove(p, de.name, DIRSIZ);
       p[DIRSIZ] = 0;
-      if(stat(buf, &st) < 0){
+      if (stat(buf, &st) < 0)
+      {
         printf(1, "ls: cannot stat %s\n", buf);
         continue;
       }
-      if(strcmp(fmtname(buf),".")==0 || strcmp(fmtname(buf),"..")==0) continue;
-      if(strcmp(fmtname(buf)+(strlen(fmtname(buf))-longz),hehe)==0)
+      if (strcmp(fmtname(buf), ".") == 0 || strcmp(fmtname(buf), "..") == 0)
+        continue;
+      if (strcmp(fmtname(buf) + (strlen(fmtname(buf)) - longz), hehe) == 0)
       {
         //printf(1,"%s\n",fmtname(buf));
-        mv(fmtname(buf),temp);
+        mv(fmtname(buf), temp);
       }
       //
     }
@@ -128,24 +144,100 @@ ls(char *path,char *argv2,int longz,char *hehe)
   close(fd);
 }
 
-
-int main(int argc,char *argv[])
+void rekursi(char path[512],char desti[512])
 {
-    if(argc<=2)
-    {
-        printf(1,"Need 2 arguments!\n");
-        exit();
-    }
-    if(argv[1][0]=='*')
-    {
-        int longz=strlen(argv[1])-1;
-        char hehe[10];
-        strcpy(hehe,argv[1]+1);
-        ls(".",argv[2],longz,hehe);
-    }
-    else
-    {
-        mv(argv[1],argv[2]);
-    }
+  //printf(1,"hmm ... %s %s\n",path,desti);
+  char buf[512],*p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+  if((fd=open(path,0))<0){
+    printf(2,"rek: cannot open %s\n",path);
     exit();
+  }
+  if(fstat(fd,&st)<0){
+    printf(2,"rek: cannot stat %s\n",path);
+    close(fd);
+    exit();
+  }
+  switch(st.type){
+    case T_FILE:
+      //mv(buf,desti);
+      break;
+    case T_DIR:
+      strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+      if(de.inum == 0)
+        continue;
+      //memset(temp,'\0',sizeof(temp));
+      //strcpy(temp,argv2);
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      if(stat(buf, &st) < 0){
+        printf(1, "rek: cannot stat %s\n", buf);
+        continue;
+      }
+      if(buf[strlen(buf)-1]=='.') continue;
+      if(strcmp(fmtname(buf),".")==0 || strcmp(fmtname(buf),"..")==0) continue;
+      if(st.type==T_DIR){
+        char tempe[512],he[512],he2[512];
+        strcpy(tempe,desti);
+        //printf(1,"%s\n",tempe);
+        mkdir(strcat(tempe,buf+2));
+        strcpy(he,buf);
+        strcpy(he2,desti);
+        //printf(1,"%s %s\n",tempe,desti);
+        //printf(1,"%s-> %s\n",buf,desti);
+        rekursi(he,he2);
+        unlink(buf);
+      }
+      else if(st.type==T_FILE){
+        //printf(1,"file %s with tujuan %s\n",buf,desti);
+        char hehe[512],hehe2[512];
+        strcpy(hehe,buf);
+        //printf(1,"%s \n",hehe);
+        strcpy(hehe2,desti);
+        //printf(1,"%s\n",hehe2);
+        mv(hehe,hehe2);
+        //printf(1,"mvnya selesai\n");
+      }
+      //if(strcmp(fmtname(buf),".")==0 || strcmp(fmtname(buf),"..")==0) continue;
+      //if(strcmp(fmtname(buf)+(strlen(fmtname(buf))-longz),hehe)==0)
+      //{
+        //printf(1,"%s\n",fmtname(buf));
+        //mv(fmtname(buf),temp);
+      //}
+      //
+    }
+    break;
+  }
+  close(fd);
+}
+
+
+
+int main(int argc, char *argv[])
+{
+  if (argc <= 2)
+  {
+    printf(1, "Need 2 arguments!\n");
+    exit();
+  }
+  if(strcmp(argv[1],"*")==0){
+    rekursi(".",argv[2]);
+  }
+  if (argv[1][0] == '*')
+  {
+    int longz = strlen(argv[1]) - 1;
+    char hehe[10];
+    strcpy(hehe, argv[1] + 1);
+    ls(".", argv[2], longz, hehe);
+  }
+  else
+  {
+    mv(argv[1], argv[2]);
+  }
+  exit();
 }
