2343fe72c694cd03c8a966d3561241caec3bede3
Ferdinand Jason G <ferdinandjasong@gmail.com>
2017-12-07 18:00:54 +0700

    [pwd.c] add -P -L options and -H -V prototype

--- pwd.c
+++ pwd.c
@@ -4,10 +4,18 @@
 #include "fcntl.h"
 #include "fs.h"
 
-int main()
+int main(int argc,char *argv[])
 {
     char cwd[100];
+    int logical=0,physical=1;
     int err=open("/temp.pwd",O_RDONLY);
+    if(argv[1][0]=='-')
+    {
+        if(strcmp(argv[1],"-L")==0) {logical=1;physical=0;}
+        if(strcmp(argv[1],"-P")==0) physical=1;
+        if(strcmp(argv[1],"-H")==0) ;// version function
+        if(strcmp(argv[1],"-V")==0) ;// help function
+    }
     if(err<0) 
     {
         printf(1,"/\n");
@@ -20,6 +28,8 @@ int main()
         exit();
     }
     close(err);
-    printf(1,"%s\n",cwd);
+    // di xv6 tidak ada symlink.    
+    if(logical==1 || physical==1 || logical==0)
+        printf(1,"%s\n",cwd);
     exit();
 }
